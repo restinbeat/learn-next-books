7517c8f9bef07396d0a3aa1a4e2f7536d313aed9
orgi-kjjung <thek226@gmail.com>
2025-01-02 10:27:36 +0900

    chore: add dependencies

--- section02/next.config.mjs
+++ section02/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  webpack5: true,
+  webpack: (config) => {
+    config.cache = false;
+    return config;
+  }
 };
 
 export default nextConfig;
