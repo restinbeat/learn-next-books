f06bd83705b65e1162dc4713c56aa141e1c624be
orgi-kjjung <thek226@gmail.com>
2025-01-06 14:17:19 +0900

    chore: delete devDependencies

--- section02/next.config.mjs
+++ section02/next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: false,
-	webpack: (config, { dev }) => {
-		if (dev) {
-			config.cache = false;
-		}
-		return config;
-	},
+  reactStrictMode: false,
+  outputFileTracing: true, // 빌드 정보 로깅 활성화
 };
 
 export default nextConfig;
