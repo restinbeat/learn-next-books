4815cab1f8cf3414f921c509d53d7be950538066
orgi-kjjung <thek226@gmail.com>
2025-01-08 14:30:23 +0900

    chore: add package.json

--- app-router/eslint.config.mjs
+++ app-router/eslint.config.mjs
@@ -1,23 +1,43 @@
+import { FlatCompat } from '@eslint/eslintrc';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
 
+// __dirname과 __filename 설정
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// FlatCompat 인스턴스 생성
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  'plugin:prettier/recommended', // Prettier 관련 규칙 추가
   {
-    // 이 부분이 추가됩니다!
+    plugins: ['prettier'], // Prettier 플러그인 적용
     rules: {
+      // TypeScript 관련 규칙
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
+
+      // 선언되지 않은 변수 에러 규칙 추가
+      'no-undef': 'error',
+
+      // Prettier 설정
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          semi: true,
+          useTabs: true,
+          tabWidth: 2,
+          trailingComma: 'all',
+          printWidth: 80,
+          bracketSpacing: true,
+          arrowParens: 'avoid',
+        },
+      ],
     },
   },
 ];
-
-export default eslintConfig;
