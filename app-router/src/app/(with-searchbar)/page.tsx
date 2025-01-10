7eab75832f80584b6ebb5e9409606ce6a131d7e0
orgi-kjjung <thek226@gmail.com>
2025-01-10 10:33:42 +0900

    feat: data cache

--- app-router/src/app/(with-searchbar)/page.tsx
+++ app-router/src/app/(with-searchbar)/page.tsx
@@ -1,21 +1,66 @@
 import BookItem from '@/components/BookItem';
 import style from './page.module.css';
-import books from '@/mock/books.json';
+import { BookData } from '@/types';
+
+async function Books() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+    { cache: 'no-store' }
+    /*
+      next.js (data cache)
+
+      { cache: 'force-cache' }
+      { cache: 'no-store' } : cache skip
+      { next: { revalidate: 10 } } : *s re fetch
+      { next: { tags: ['a'] } } : on-demand revalidate (요청이 들어왔을 때 최신화)
+
+     */
+  );
+  const books: BookData[] = await response.json();
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다.</div>;
+  }
+
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+async function RandomBooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 3 } }
+  );
+  const randomBooks: BookData[] = await response.json();
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다.</div>;
+  }
+
+  return (
+    <div>
+      {randomBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
 
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <RandomBooks />
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <Books />
       </section>
     </div>
   );
