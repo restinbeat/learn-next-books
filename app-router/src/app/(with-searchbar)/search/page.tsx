43f8bbbcefa94af3912b90ebbcfc17bdbd88d3fb
orgi-kjjung <thek226@gmail.com>
2025-01-16 15:20:38 +0900

    feat: route segment option

--- app-router/src/app/(with-searchbar)/search/page.tsx
+++ app-router/src/app/(with-searchbar)/search/page.tsx
@@ -1,6 +1,13 @@
 import BookItem from '@/components/BookItem';
 import { BookData } from '@/types';
 
+// 특정 페이지에 유형을 강제로 Static, Dynamic 페이지로 설정
+// 1. auto : default option, 아무것도 강제하지 않음
+// 2. force-dynamic : 페이지를 강제로 Dynamic 페이지로 설정
+// 3. force-static :  페이지를 강제로 Static 페이지로 설정
+// 4. error : 페이지를 강제로 Static 페이지로 설정 (설정하면 안되는 이유 -> 빌드 오류)
+// export const dynamic = 'auto';
+
 export default async function Page(props: {
   searchParams: Promise<{
     q?: string;
