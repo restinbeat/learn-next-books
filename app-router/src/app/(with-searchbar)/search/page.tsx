8b586070947ee86e291f6c9ab56516d98dfddc44
restinbeat <thek226@gmail.com>
2025-02-03 14:02:06 +0900

    feat: searchbar (suspense)

--- app-router/src/app/(with-searchbar)/search/page.tsx
+++ app-router/src/app/(with-searchbar)/search/page.tsx
@@ -1,6 +1,7 @@
 import BookItem from '@/components/BookItem';
 import { BookData } from '@/types';
 import { delay } from '@/util/delay';
+import { Suspense } from 'react';
 
 // 특정 페이지에 유형을 강제로 Static, Dynamic 페이지로 설정
 // 1. auto : default option, 아무것도 강제하지 않음
@@ -9,17 +10,11 @@ import { delay } from '@/util/delay';
 // 4. error : 페이지를 강제로 Static 페이지로 설정 (설정하면 안되는 이유 -> 빌드 오류)
 // export const dynamic = 'auto';
 
-export default async function Page(props: {
-  searchParams: Promise<{
-    q?: string;
-  }>;
-}) {
-  const searchParams = await props.searchParams;
-
+async function SearchResult({ q }: { q: String }) {
   // 개발자 도구 3G로 변경해도됌
   await delay(1500);
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`,
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
     { cache: 'force-cache' }
   );
   if (!response.ok) {
@@ -34,5 +29,19 @@ export default async function Page(props: {
         <BookItem key={book.id} {...book} />
       ))}
     </div>
+  );
+}
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams: {
+    q?: string;
+  };
+}) {
+  return (
+    <Suspense key={searchParams.q || ''} fallback={<div>Loading ...</div>}>
+      <SearchResult q={searchParams.q || ''} />
+    </Suspense>
   );
 }
